4ddcf7ce41a350933e233d4580a5e0274a8c44a2
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-11-30 03:44:50 +0530

    Making changes to deploy

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/./frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+    res.sendFile(path.join(`${__dirname}/./frontend/build/index.html`));
   });
 }
 const port =  process.env.PORT || 5000
