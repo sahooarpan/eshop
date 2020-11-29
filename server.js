f19de48f06a2bec2f2d79a5b9f4c38fee32d81f1
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-11-29 17:56:10 +0530

    Final Commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRoute = require("./routes/userRouter");
 const productRoute = require("./routes/productRoute");
 const orderRoute = require("./routes/orderRoute");
 const cors = require("cors");
-
+const path = require('path');
 connectDB();
 dotenv.config();
 app.use(bodyParser.json());
@@ -15,6 +15,12 @@ app.use(cors());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+}
 app.listen("5000", () => {
   console.log(`Server started`);
 });
